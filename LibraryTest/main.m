9183c6f4ed56ebca1f34d5c2daadc15df6100924
Sean Eric Fagan <kithrup@mac.com>
2021-09-30 13:42:57 -0700

    Initial Commit

---
+++ LibraryTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LibraryTest
+//
+//  Created by Sean Eric Fagan on 9/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
